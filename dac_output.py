1aa9d3ee68d37978195cf79648edddb5b5d87f99
amanda-hegadorn <71232613+amanda-hegadorn@users.noreply.github.com>
2024-02-14 16:09:55 -0500

    Add files via upload

--- dac_output.py
+++ dac_output.py
@@ -1,23 +1,59 @@
+import csv
 import time
-import pandas as pd
-import Adafruit_MCP4725
+import MCP4725
 import smbus2
-import pi_MCP4725
 
+# Initialize the DAC
+dac = MCP4725.MCP4725(smbus2.SMBus(1), 0x62, smbus2)
 
-dac = pi_MCP4725.MCP4725(smbus2.SMBus(1), 0x62,smbus2)
-while True:
+# Specify the file name
+fileName = 'data_2024-02-14.csv'
 
-    for x in range(0,4097,150):
-
+# Open and read the CSV file
+with open(fileName, mode='r') as file:
+    csvReader = csv.DictReader(file)
+    currenttime_ms=0
+    for row in csvReader:
+        # Extract the voltage (mV) value from the CSV
+        lasttime_ms=currenttime_ms
+        currenttime_ms=float(row["Time (ms)"])
+        voltage_mV = float(row['Voltage (mV)'])
         
-        print(x)
+        # Convert mV to V for the DAC calculation
+        voltage_V = voltage_mV / 1000.0
+        
+        # Assuming your DAC output range is 0 to 5V, calculate the DAC value
+        # Adjust the formula according to your DAC's resolution and voltage range if necessary
+        dac_value = int((voltage_V / 5.0) * 4095)
+        
+        # Clamp the DAC value to the valid range [0, 4095] just in case
+        dac_value = max(0, min(4095, dac_value))
+        
+        # Write the value to the DAC
+        dac.write(dac_value)
+        
+        print(f"Setting DAC value to: {dac_value}, which corresponds to Analog Voltage: {voltage_V:.2f} V")
+        
+        # Delay to simulate real-time data playback, adjust as needed
+        time.sleep((currenttime_ms-lasttime_ms)/1000)  # Adjust this delay to match the sampling rate of your original data if needed
 
-        dac.write(x)
 
+#########################################################################
 
-        voltage = x/4096.0*5.0
+# import time
+# import smbus2
+# import MCP4725
+# import csv
 
-        print("\nAnalogVolt: %.2f\n" % voltage)
+# dac = MCP4725.MCP4725(smbus2.SMBus(1), 0x62,smbus2)
 
-        time.sleep(0.001)
+# fileName='data_2024-02-14.csv'
+
+# while True:
+#     for x in range(0,4097,150):
+#         print(x)
+#         dac.write(x)
+
+#         voltage = x/4096.0*5.0
+#         print("\nAnalogVolt: %.2f\n" % voltage)
+#         time.sleep(0.0001)
--- main.py
+++ main.py
@@ -2,76 +2,159 @@ import ADS1115
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import time
-import pandas as pd
 import smbus2
 import RPi.GPIO as GPIO
+import datetime
+import csv
+
+# Button Event Setting
+__GPIO_BUTTON_START__ = 11
+__GPIO_BUTTON_FINISH__ = 13
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup([__GPIO_BUTTON_START__, __GPIO_BUTTON_FINISH__], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# Initialize placeholders
+save_data = False  # Flag to control data saving
+data = []  # List to hold the data
+
+# Callback functions for GPIO button events
+def startDataCollecting(channel):
+    global save_data
+    print("Starting to save data")
+    save_data = True
 
 
-# are we saving data right now
-save_data = False
+def finishDataCollecting(channel):
+    global save_data, data
+    filename = "data_{0}.csv".format(datetime.date.today())
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Time (ms)", "Voltage (mV)"])  # Write header
+        writer.writerows(data)
+    print("Saved data")
+    data = []  # Clear data after saving
+    save_data = False
+
+# Setup GPIO event detection
+GPIO.add_event_detect(__GPIO_BUTTON_START__, GPIO.RISING, callback=startDataCollecting, bouncetime=200)
+GPIO.add_event_detect(__GPIO_BUTTON_FINISH__, GPIO.RISING, callback=finishDataCollecting, bouncetime=200)
 
 # Initialize the ADS1115 ADC
 ads = ADS1115.ADS1115()
 ads.setADCConfig(sps=860)
 
-# Number of data points
+# Plotting setup
 x_len = 30
 y_range = [0, 5000]
-
-# Create figure for plotting
 fig, ax = plt.subplots()
 xs = list(range(0, x_len))
 ys = [0] * x_len
 ax.set_ylim(y_range)
-
-# Create a line, which we will update with new data
 line, = ax.plot(xs, ys)
 start_time = time.time()
-# Define a function for the animation
 
-#create pandas object
-data={"Time":[],"Voltage (mV)":[]}
-
-def my_callback(channel):
-    if GPIO.event_detected(channel):
-        if(save_data):
-            df = pandas.DataFrame.from_dict(data)
-            df.to_csv("data_{datetime.date.today()}.csv")
-            print("Saved data")
-        if(not save_data):
-            print("starting to save data")
-        save_data= not save_data
-
-GPIO.add_event_detect(11, GPIO.RISING, callback=my_callback)  # add rising edge detection on a channel
-
+# Animation function
 def animate(i, ys):
-    # Read voltage from ADS1115
+    global data
     volt = ads.readADC()
-    x_val=start_time-time.time()
-    #print(time.time())
-    #print("{:.0f} mV measured on AN0".format(volt))
+    x_val = (time.time() - start_time)*1000
 
-    # Add y to list
     ys.append(volt)
-    if(save_data):
-        data["Time"].append(x_val)
-        data["Voltage (mV)"].append(volt)
+    if save_data:
+        data.append([x_val, volt])
 
-    # Limit y list to set number of items
     ys = ys[-x_len:]
-
-    # Update line with new Y values
     line.set_ydata(ys)
-
-    #print(i)
     return line,
 
-# Set up plot to call animate() function periodically (every 1 ms, this is probably limiting our sampling frequency as well)
-ani = animation.FuncAnimation(fig, animate, fargs=(ys,), interval=.01, blit=True)
+ani = animation.FuncAnimation(fig, animate, fargs=(ys,), interval=1, blit=True)
 plt.show()
+
+#############################################################################
+# import ADS1115
+# import matplotlib.pyplot as plt
+# import matplotlib.animation as animation
+# import time
+# import pandas as pd
+# import smbus2
+# import RPi.GPIO as GPIO
+# import datetime
+# import csv
+
+# # Button Event Setting
+# __GPIO_BUTTON_START__=11
+# __GPIO_BUTTON_FINISH__=13
+
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup([__GPIO_BUTTON_START__,__GPIO_BUTTON_FINISH__], GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# GPIO.add_event_detect(__GPIO_BUTTON_START__, GPIO.RISING, callback=startDataCollecting)
+# GPIO.add_event_detect(__GPIO_BUTTON_FINISH__, GPIO.RISING, callback=finishDataCollecting)
+
+# def startDataCollecting():
+#     global save_data
+#     print("starting to save data")
+#     save_data=True
+
+# def finishDataCollecting():
+#     global save_data
+#     global df
+#     df = pd.DataFrame.from_dict(data)
+#     df.to_csv("data_{0}.csv".format(datetime.date.today()),index=False)
+#     print("Saved data")
+#     save_data=False
+        
+
+
+# # are we saving data right now
+# save_data=False
+
+# # Initialize the ADS1115 ADC
+# ads = ADS1115.ADS1115()
+# ads.setADCConfig(sps=860)
+
+# # Number of data points
+# x_len = 30
+# y_range = [0, 5000]
+
+# # Create figure for plotting
+# fig, ax = plt.subplots()
+# xs = list(range(0, x_len))
+# ys = [0] * x_len
+# ax.set_ylim(y_range)
+
+# # Create a line, which we will update with new data
+# line, = ax.plot(xs, ys)
+# start_time = time.time()
+
+# #create pandas object
+# data={"Time":[],"Voltage (mV)":[]}
+
+
+# # Set up plot to call animate() function periodically (every 1 ms, this is probably limiting our sampling frequency as well)
+# ani = animation.FuncAnimation(fig, animate, fargs=(ys,), interval=.01, blit=True)
+# plt.show()
+
+# def animate(i, ys):
+#     # Read voltage from ADS1115
+#     volt = ads.readADC()
+#     x_val=start_time-time.time()
+
+#     # Add y to list
+#     ys.append(volt)
+#     if(save_data):
+#         data["Time"].append(x_val)
+#         data["Voltage (mV)"].append(volt)
+
+#     # Limit y list to set number of items
+#     ys = ys[-x_len:]
+
+#     # Update line with new Y values
+#     line.set_ydata(ys)
+#     print(save_data)
+#     return line,
+
+
 
 
 
